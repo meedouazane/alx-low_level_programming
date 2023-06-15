4fb2aea2528743e2908494c932a1ace5c09ff407
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-15 07:51:28 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,6 @@
 int main(void)
 {
 
-/**
-* printf function to print
-*
-*/
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long));
