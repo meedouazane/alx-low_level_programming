60109646b14612cc89990ff889cc6c6164fa6c2e
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-16 13:41:22 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int b = 0;
+	for (; i < 10; i++)
+	{
+		for (b = i+1; b < 10; b++)
+		{
+			if (b != i)
+			{
+			putchar(i + '0');
+			putchar(b + '0');
+				if (i == 8 && b == 9)
+				{
+					break;
+				}
+				else
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
