ab752853871f64ae3eac9ebd45c3b75e6363a01c
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-16 07:17:11 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+	while(alphabets[i] != 0){
+		putchar(alphabets[i]);
+		i++;
+	}
+	return (0);
+}
