4f706900066e3947d0dede69573506a70b901758
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-16 09:27:44 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets[] = "abcdfghijklmnoprstuvwxyz\n";
+	int i = 0;
+
+	while (alphabets[i] != 0)
+	{
+		putchar(alphabets[i]);
+		i++;
+	}
+	return (0);
+}
