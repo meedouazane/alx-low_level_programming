4d231921e6ac7e3a55d2863169f7f136f32c7ccd
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-16 09:35:55 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return : Always 0 (Seccess)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i<10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return(0);	
+}	
