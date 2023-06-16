a7d94dc2b5cf814f6ce7e66a98a3cdee2ac4f2da
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-16 13:57:49 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int b ;
+	int c ;
+
+	for (; i < 10; i++)
+	{
+		for (b = i + 1; b < 10; b++)
+		{
+			for (c = b+1; c < 10; c++)
+			{
+			if (b != i)
+				{
+			putchar(i + '0');
+			putchar(b + '0');
+			putchar(c + '0');
+				if (i == 7 && b == 8 && c == 9)
+					{
+					break;
+					}
+				else
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
