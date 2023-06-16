4fa0a5b2376691924658371900353f636ff3a7e0
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-16 07:20:00 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	char alphabets[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i = 0;
-	
-	while(alphabets[i] != 0)
+
+	while (alphabets[i] != 0)
 	{
 		putchar(alphabets[i]);
 		i++;
