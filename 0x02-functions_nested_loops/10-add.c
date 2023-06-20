effddda45a558a55b90b07bf94f4483bc1b5752d
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-20 11:20:08 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integres
+ *@a: first number
+ *@b: second number
+ * Return: results of adding two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
