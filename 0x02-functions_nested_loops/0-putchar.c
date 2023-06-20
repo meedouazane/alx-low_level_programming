edd224a4be66423d2aee484058b386887d16886c
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-20 04:25:27 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char put[] = "_putchar\n";
+	int i = 0;
+
+	while (put[i] != 0)
+	{
+	putchar(put[i]);
+	i++;
+	}
+
+return (0);	
+}	
