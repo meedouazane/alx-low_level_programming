099e69cf5ebcc7480e05c713211974a0c43bd87d
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-21 11:33:55 -0700

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	unsigned long i, b, c, n;
+	 long i, b, c, n;
 
 	c = 1;
 	b = 0;
-	for (i = 2; i < 50; i++)
+	for (i = 2; i <= 50; i++)
 	{
 		n = c + b;
 		b = c;
 		c = n;
-		printf("%lu, ", c);
+		printf("%ld, ", c);
 	}
 	printf("\n");
 	return (0);
