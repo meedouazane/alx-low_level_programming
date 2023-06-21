f9fc0f81803a3a6ccc68ce7054e16340097748f2
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-20 17:03:47 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = i * b;
-			if (c <= 9 && b != 9)
+			if (c <= 9 && b <= 9)
 			{
 			_putchar(c + '0');
 			_putchar(',');
@@ -20,7 +20,7 @@ void times_table(void)
 			_putchar(' ');
 			}
 			else
-				if (c >= 9 && b != 9)
+				if (c >= 9 && b < 9)
 				{
 				t = c / 10;
 				f = c % 10;
@@ -31,7 +31,7 @@ void times_table(void)
 				}
 				else
 				{
-					if (c <= 9 && b != 9)
+					if (c <= 9 && b < 9)
 					_putchar(c + '0');
 					else
 					if (c > 9 && b == 9)
