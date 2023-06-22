eb76ebadfd01d9a902c54ac77021e79836e14cea
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-22 08:32:42 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int i, b;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (b = 1; b <= 14; b++)
 		{
 			if (b >= 10)
 				_putchar((b / 10) + '0');
