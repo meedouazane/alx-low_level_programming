32a5c6759399b795f24bd585628b905c2174fd3d
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-22 02:46:58 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,17 @@
 
 /**
  * _isupper - checks for uppercase character
+ *
  * @c : character that we'll cheack
- * return- 0 if it lower 1 for uper
+ *
+ * Return - return 0 if it lowercase and return 1 for upercase.
+ *
  */
 
 int _isupper(int c)
 {
 
-	if (c >= 65 && c <=90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
