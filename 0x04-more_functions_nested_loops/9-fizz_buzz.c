f9996b766e0a27984092102580cc722e911bb856
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-22 14:24:55 -0700

    99

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,12 +10,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0 && i != 100)
-			printf(" FizzBuzz ");
-			else if (i % 5 == 0 && i % 3 != 0 && i != 100)
-			printf(" Buzz ");
-			else if (i % 3 == 0 && i % 5 != 0 && i != 100) 
-			printf(" Fizz ");
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+			else if (i % 5 == 0)
+			printf("Buzz ");
+			else if (i % 3 == 0)
+			printf("Fizz ");
+		if (i % 3 == 0 || i % 3 == 0 || i % 5 == 0)
+			continue;
 		else
 		printf("%d ", i);
 	}
