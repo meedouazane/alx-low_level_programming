92e5cceef6221f9d751cdb7b6ca272ce4c89a6e1
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-22 09:27:38 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,12 @@
 void print_line(int n)
 {
 	int i;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (i = 1; i <= n; i++ )	
+		for (i = 1; i <= n; i++)
 			_putchar('_');
 		_putchar('\n');
 	}
