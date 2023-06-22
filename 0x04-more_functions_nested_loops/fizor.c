a28ec61fe8fa1b489375e0bacb2e5a4b6a739ce7
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-22 14:51:47 -0700

    99

--- 0x04-more_functions_nested_loops/fizor.c
+++ 0x04-more_functions_nested_loops/fizor.c
@@ -12,12 +12,12 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf(" FizzBuzz ");
-			else if (i % 5 == 0)
+			else if (i % 5 == 0 && i % 5 != 100 )
 			printf(" Buzz ");
 			else if (i % 3 == 0)
 			printf(" Fizz ");
-		if (i % 3 == 0 || i % 3 == 0 || i % 5 == 0)
-			continue;
+		if (i % 5 == 100)
+			printf("Buzz");
 		else
 		printf("%d ", i);
 	}
