a8be324ff347bc72a69af9d137d188cf18ccfd5d
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-22 13:33:05 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * main - print fizz buzz program
  * Return: Always 0 (Success)
@@ -10,16 +10,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf(" FizzBuzz ");
-			else if (i % 5 == 0)
-			printf(" Buzz ");
-			else if (i % 3 == 0)
-			printf(" Fizz ");
-		if (i % 3 == 0 || i % 3 == 0 || i % 5 == 0)
-			continue;
-		else
-		printf("%d ", i);
+	if ((i % 3 == 0) && (i % 5 == 0))
+	printf(" FizzBuzz ");
+	else if (i % 5 == 0)
+	printf(" Buzz ");
+	else if (i % 3 == 0)
+	printf(" Fizz ");
+	if (i % 3 == 0 || i % 3 == 0 || i % 5 == 0)
+	continue;
+	else
+	printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
---
+++ 0x04-more_functions_nested_loops/fizor.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - print fizz buzz program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz ");
+			else if (i % 5 == 0)
+			printf(" Buzz ");
+			else if (i % 3 == 0)
+			printf(" Fizz ");
+		if (i % 3 == 0 || i % 3 == 0 || i % 5 == 0)
+			continue;
+		else
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
+
