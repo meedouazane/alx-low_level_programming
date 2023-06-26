5f697f5c3269ced0f11106d05a0533348b72869a
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-26 08:03:11 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,8 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
-	while (*s != 0)
-	{
+	for (i = 0; s[i] != 0; i++)
 		length++;
-		s++;
-	}
 
 	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
