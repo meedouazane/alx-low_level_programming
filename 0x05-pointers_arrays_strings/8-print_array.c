952e54f7634fb848c443e1ea09419e5a4736f286
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-26 09:30:55 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@ void print_array(int *a, int n)
 	int i = 0;
 
 	for (i = 0; i <= n - 1; i++)
-	{
+	{	
+		printf("%d", a[i]);
 		if (i != n - 1)
-		printf("%d, ", a[i]);
-		if (i == n - 1)
-		printf("%d\n", a[i]);
+		printf(" ,");
 	}
+	printf("\n");
 }
