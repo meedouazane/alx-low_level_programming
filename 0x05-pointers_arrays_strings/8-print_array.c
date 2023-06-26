8dbdbb91339ac6e8bb4794ac6afe85247ab739ca
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-26 09:17:26 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - print array
+ * @n: number of elements of the array
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i != n - 1)
+		printf("%d, ", a[i]);
+		if (i == n - 1)
+		printf("%d\n", a[i]);
+	}
+}
