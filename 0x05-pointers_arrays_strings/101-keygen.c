38aac162e2c4a316bf80d851cf1025a2c060efe6
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-27 08:47:09 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entery point
+ *
+ * return: always 0 (Success)
+ */
+
+int main(void)
+{
+	srand(time(NULL));
+	int i = 0;
+	char random[10];
+
+	for (i = 0; i < 10; i++)
+	{
+		random[i] = rand() % 26 + '0';
+		printf("%c", random[i]);
+	}
+	return (0);
+}
