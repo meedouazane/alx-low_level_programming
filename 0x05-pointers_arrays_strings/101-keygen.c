8c492dec8ac119e8abd6758ada6428258edffedb
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-27 08:54:44 -0700

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	srand(time(NULL));
 	int i = 0;
-	char random[10];
+	char random[20];
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 20; i++)
 	{
 		random[i] = rand() % 26 + '0';
 		printf("%c", random[i]);
