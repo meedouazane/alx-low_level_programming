10b5695a4f9ed33c59ae631e560f2d12ccdb16a5
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-29 15:17:47 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 	int i = 0;
 for (i = 0; str[i] != 0; i++)
 {
-if (str[i] == 32 || str[i] == 9 || str[i] == 10 || str[i] == 46)
+if ((str[i] >= 32 && str[i] <= 35) || str[i] == 9 || str[i] == 10 || str[i] == 46 || str[i] == 59 || str[i] == 40 || str[i] == 41 || str[i] == 123 || str[i] == 125 || str[i] == 63)
 {
 if (str[i + 1] > 97 && str[i + 1] < 122)
 str[i + 1] = str[i + 1] - 32;
