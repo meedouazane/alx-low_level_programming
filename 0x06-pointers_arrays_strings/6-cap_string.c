5324568a2c2cf4db1fa25ddf51b432a27808b0f9
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-29 16:32:41 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,21 +5,22 @@
  * @str: string
  * Return: capitalizes value
  */
+
 char *cap_string(char *str)
 {
 	int l, i = 0;
 	char Sep[] = " ,;.\"!?(){}\t\n";
 
-for (i = 0; str[i] != 0; i++)
-{
-	for (l = 0; Sep[l] != 0; l++)
+	for (i = 0; str[i] != 0; i++)
 	{
-	if (str[i] == Sep[l])
-	{
-	if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-	str[i + 1] = str[i + 1] - 32;
+		for (l = 0; Sep[l] != 0; l++)
+		{
+			if (str[i] == Sep[l])
+			{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			str[i + 1] = str[i + 1] - 32;
+			}
+		}
 	}
-	}
-}
 	return (str);
 }
