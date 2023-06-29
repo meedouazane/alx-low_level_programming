414b84ff787ff4f627c36cea604007b2d6b4f12c
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-29 05:50:19 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,24 +4,22 @@
  * _strncpy - copy two strings
  * @dest: destination string
  * @src: source string
- * @: size
+ * @n: size
  * Return: concatenated strings
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-     int i = 0;
-          
-          
-          for (i = 0; src[i] != 0 && i < n; i++)
-	  {
-                dest[i] = src[i];
-	  }     
-                while (i < n)
-                {
-                dest[i] = 0;
-                 i++;
-                }
-                
-                return (dest);
+	int i = 0;
+
+	for (i = 0; src[i] != 0 && i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	while (i < n)
+	{
+	dest[i] = 0;
+	i++;
+	}
+	return (dest);
 }
 
