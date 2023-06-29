71dd3e7f48e1a7060b8becb62463c8c09b3f91c0
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-29 16:03:51 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,23 @@
  */
 char *cap_string(char *str)
 {
-	int l, i = 0;
-	char Sep[] = " ,;.\"!?(){}\t\n";
+int i = 0;
+char Sep[] = " ,;.\"!?(){}\t\n";
 
-for (i = 0; str[i] != 0; i++)
+while (str[i] != '\0')
 {
-	for (l = 0; Sep[l] != 0; l++)
-	{
-	if (str[i] == Sep[l])
-	{
-	if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-	str[i + 1] = str[i + 1] - 32;
-	}
-	}
+int l;
+for (l = 0; Sep[l] != '\0'; l++)
+{
+if (str[i] == Sep[l])
+{
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+str[i + 1] = str[i + 1] - 32;
 }
-	for (i = 0; str[i] != 0; i++)
-	{
-	if (str[i] == '\t')
-	str[i] = ' ';
-	}
-
-	return (str);
+}
+if (str[i] == '\t')
+str[i] = ' ';
+i++;
+}
+return (str);
 }
