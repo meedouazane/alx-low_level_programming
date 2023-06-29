c530a23bfac9b416e09769d2359c17d053488954
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-29 05:37:58 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * 
+ * @src: source string
+ * 
+ * Return: Always 0
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		s[i] = s[i] - 32;
+	}
+	return (s);
+}
