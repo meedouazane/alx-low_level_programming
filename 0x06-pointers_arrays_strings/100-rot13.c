717f426ac9187eab057adb51edaeb8c9f9867d6d
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-06-30 09:14:14 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,19 +5,16 @@
  * @str: string
  * Return: encoded value
  */
-
 char *rot13(char *str)
 {
-int i;
+int i = 0;
 
 for (i = 0; str[i] != '\0'; i++)
 {
-if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-{
-char base = (str[i] >= 'a' && str[i] <= 'z') ? 'a' : 'A';
-str[i] = ((str[i] - base + 13) % 26) + base;
+if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+str[i] = (str[i] + 13);
+else if ((str[i] >= 'm' && str[i] <= 'z') || (str[i] >= 'M' && str[i] <= 'z'))
+str[i] = (str[i] - 13);
 }
-}
-
-return str;
+return (str);
 }
