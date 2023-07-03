960ab38ef2e43454baaa04bc6b7f0f5ffdff1b8e
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-03 05:24:52 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strspn - gets the length of a prefix substring
- * @s: the initial segment 
+ * @s: the initial segment
  * @accept: consist only of bytes
  * Return: number of bytes in the intial segments
  */
@@ -14,18 +14,13 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
-			{
-			if (s[i] == accept[j])
-			{
-				break;
-			}
-			}
-			if (accept[j] == '\0')
-			{
-				break;
-			}
-
-			t++;
+		{
+		if (s[i] == accept[j])
+		break;
+		}
+		if (accept[j] == '\0')
+		break;
+		t++;
 	}
-	return t;
+	return (t);
 }
