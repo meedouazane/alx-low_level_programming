7cbd8bc7e8b9fa49f0122de7711f24e9a80c815e
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-05 08:56:59 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * _sqrt- check for root square from 1.
+ *_sqrt- check for root square from 1.
  * @i: the starting number to check.
  * @n: input number
  * Return: the natural square root of a number
  */
-
 int _sqrt(int n, int i)
 {
 	if (i * i > n)
@@ -17,13 +15,11 @@ int _sqrt(int n, int i)
 	return (_sqrt(n, i + 1));
 
 }
-
-/*
+/**
  *_sqrt_recursion- function that returns the natural square root of a number.
- *@n: input number
- *Return: the natural square root of a number
+ * @n: input number
+ * Return: the natural square root of a number
  */
-
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
