c26ae11b95c96be2296a0237e818c9cc4baa6181
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-06 06:26:29 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,35 @@
 #include "main.h"
 /**
  * palindrome- compaire two strings one of them is backword.
- * @start: starting from the first digit 0.
- * @end: starting from last digit.
+ * start: starting from the first digit 0.
+ * end: starting from last digit.
+ * @s: input string
  * return: 0 or 1 if it's palindrome.
  */
-
 int _strlen_recursion(char *s);
 int is_palindrome(char *s);
 
-int palindrome(char *s, int start, int end) {
-    if (start >= end)
-        return 1;
-
-    if (s[start] != s[end])
-        return 0;
-    
-    return palindrome(s, start + 1, end - 1);
+int palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+		return (palindrome(s, start + 1, end - 1));
 }
+
+/**
+ * is_palindrome- returns 1 if a string is a palindrome and 0 if not.
+ * @s: input string.
+ * Return: returns 1 if a string is a palindrome, 0 if it's not.
+ */
 
 int is_palindrome(char *s)
 {
- int end = _strlen_recursion(s) - 1;
- int start = 0;
- return palindrome(s, start, end);
+	int end = _strlen_recursion(s) - 1;
+	int start = 0;
+
+	return (palindrome(s, start, end));
 }
 
 /**
@@ -34,7 +40,7 @@ int is_palindrome(char *s)
 
 int _strlen_recursion(char *s)
 {
-    int i = 0;
+	int i = 0;
 
 	if (*s != '\0')
 	{
