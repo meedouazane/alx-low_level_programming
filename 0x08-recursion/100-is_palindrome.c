386634aa559c206a1d893a4193eab10959e4b4af
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-06 06:15:02 -0700

    0

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,46 @@
 #include "main.h"
+/**
+ * palindrome- compaire two strings one of them is backword.
+ * @start: starting from the first digit 0.
+ * @end: starting from last digit.
+ * return: 0 or 1 if it's palindrome.
+ */
 
-int _strlen_recursion(char *s)
-{
-   static int i = 0;
+int _strlen_recursion(char *s);
+int is_palindrome(char *s);
 
-    if (*s != '\0')
-    {
-        i++;
-        return 1 + _strlen_recursion(s + 1);
-    }
-    return i;
+int palindrome(char *s, int start, int end) {
+    if (start >= end)
+        return 1;
+
+    if (s[start] != s[end])
+        return 0;
+    
+    return palindrome(s, start + 1, end - 1);
 }
 
 int is_palindrome(char *s)
 {
-    int length = _strlen_recursion(s);
+ int end = _strlen_recursion(s) - 1;
+ int start = 0;
+ return palindrome(s, start, end);
+}
+
+/**
+ * _strlen_recursion- returns the length of a string
+ * @s: input string
+ * Return: length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
     int i = 0;
 
-    if (s[i] == s[length - i - 1]) {
-        return 0;
-        return is_palindrome(s + 1);
-    }
-    else {
-        return 1;
-    }
+	if (*s != '\0')
+	{
+	i++;
+	return (1 +  _strlen_recursion(s + 1));
+	}
+	return (i);
 }
+
