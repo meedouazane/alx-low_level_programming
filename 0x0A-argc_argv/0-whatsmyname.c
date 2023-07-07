eb1a1e4fd1e59a524a963b3ab256200a0062ddc2
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-07 05:28:22 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main- entery of the program
+ * @argc:number of arguments being passed into a program
+ * @argv:array of arguments
  * Return: 0 (Success)
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
