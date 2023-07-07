bf3c0e03ad30432ad982535d81f91419505a293c
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-07 05:24:38 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- entery of the program
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
