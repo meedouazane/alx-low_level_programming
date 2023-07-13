0482f52a9c93354d97acc9a45d3bebcda5a8cae5
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-13 15:56:29 -0700

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,26 +6,38 @@
  *  @new_size: the new size in bytes.
  *  Return: Always 0
  */
+char *_memcpy(char *dest, char *src, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *k;
+	void *k;
+	unsigned int i = 0;
 
-	ptr = malloc(old_size);
 	if (ptr == NULL)
-		ptr = malloc(new_size);
+	{
+		k = malloc(new_size);
+		if (k == NULL)
+			return (NULL);
+	return (k);
+	}
 
-if (ptr != NULL && new_size == 0)
-{
-free(ptr);
-return (NULL);
-}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-if (new_size == old_size)
-return (ptr);
-else
-{
-k = malloc(new_size);
-ptr = k;
-}
-return (ptr);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size > old_size)
+	{
+		k = malloc(new_size);
+		if (k == NULL)
+			return(NULL);
+		for (; i < new_size; i++)
+		{
+			*((char *)k + i) = *((char *)ptr + i);
+		}
+		free(ptr);
+	}
+return (k);
 }
