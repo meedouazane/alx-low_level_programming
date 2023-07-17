4cdc54c4bacac5bb17c57c3b39cf2364b9f2fed0
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-17 11:10:17 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	if (!name || age < 0 || !owner)
+		return (NULL);
 
 	new_dog = malloc(sizeof(struct dog));
 		if (new_dog == NULL)
@@ -16,6 +18,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
-
 	return (new_dog);
 }
