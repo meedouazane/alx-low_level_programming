794418a86dc6bfd696b5751a6341d6ec4398d67f
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-17 07:06:04 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,9 +6,7 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL && d->age == 0.0 && d->owner == NULL)
-	printf("%s", "");
-	else
+	if (d)
 	{
 	printf("Name: %s\n", d->name ? d->name :"(nil)");
 	printf("Age: %.6f\n", d->age);
