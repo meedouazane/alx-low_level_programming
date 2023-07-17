1877bda4e4ea49e4685d084bed85b24fce7d0f96
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-17 11:00:40 -0700

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,7 @@ struct dog
 };
 
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #include <stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #include <stdlib.h>
