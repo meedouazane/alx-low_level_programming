8977dcae800cd9c6070aa84511ab1ebc92e0971f
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-19 04:46:15 -0700

    0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_h
+#define FUNCTION_POINTERS_h
+int _putchar(char c);
+#include <stdio.h>
+#include <string.h>
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_h */
