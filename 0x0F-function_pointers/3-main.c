f08ceeaabf8dead39a65d0fb51861cf2b47433bc
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-19 10:09:03 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,24 +5,24 @@
  *@argv: array of arguments.
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int a, b = 0;
 	int result = 0;
-	int (*op_func)(int, int);
+	int (*op)(int, int);
 	
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
+	op = get_op_func(argv[2]);
+	if (!op)
 		printf("Error\n"), exit(99);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(99);
-	result = op_func(a, b);
+	result = op(a, b);
 	printf("%d\n", result);
 	return (0);
 }
