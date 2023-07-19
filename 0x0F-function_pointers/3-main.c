2222aac430355404dc3c003f317be28986bb66fa
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-19 09:43:10 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,16 +9,16 @@ int main(int argc, char *argv[])
 {
 	int a, b = 0;
 	int result = 0;
-	char *op;
-
+	int (*op_func)(int, int);
+	
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 
 	a = atoi(argv[1]);
-	op = argv[2];
 	b = atoi(argv[3]);
-
-	result = (*get_op_func(op))(a, b);
+	
+	op_func = get_op_func(argv[2]);
+	result = op_func(a, b);
 	printf("%d\n", result);
 	return (0);
 }
