7fb53c0fb18dc7fea9b921f162eb1c9c03af01ef
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-19 09:50:29 -0700

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
 
 	for (i = 0; i < 5; i++)
 	{
-	if (s[0] == ops[i].op[0])
+	if (s && s[0] == ops[i].op[0] && !s[i])
 	return (ops[i].f);
 	}
 	return (NULL);
