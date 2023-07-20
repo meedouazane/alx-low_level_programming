658c5dc307b70441ca80a73cf474067a443884cd
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-20 11:33:00 -0700

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all -  sum of all the parameters.
  * @n: number of parametres.
- * Return: Always 0
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -19,7 +19,9 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	else
 		return (0);
+	va_end(ar);
 	return (sum);
+
 }
 
 
