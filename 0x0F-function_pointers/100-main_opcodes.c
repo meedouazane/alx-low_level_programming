82c13324941f2f2943bf42f6e3539accf1906fef
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-20 11:39:55 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 /**
  * main - check the code
- *
+ *@argc: number of arguments.
+ *@argv: array of arguments.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
