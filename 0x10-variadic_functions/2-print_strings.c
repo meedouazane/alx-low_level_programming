1853bfbdeab1c4a7b79c51d120eda96ab1d29f9d
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-07-20 04:40:26 -0700

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,8 +21,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 		{
 			printf("%s", str);
-			if (i < n - 1 && separator != NULL)
-				printf("%c", *separator);
+			if (i < n - 1 && *separator)
+				printf("%s", separator);
 		}
 	}
 	va_end(ar);
