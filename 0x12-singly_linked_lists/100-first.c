978191b6271d3a0d27d9214a2d09cc3f691360c5
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-07-28 05:08:13 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * before_main - function that executed before main
+ *
+ * Return: Always 0.
+ */
+void before_main(void) __attribute__((constructor));
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
