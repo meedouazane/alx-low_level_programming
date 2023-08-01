32fe8d3db179cef9e0e015057bcc87cf77261057
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-01 11:57:35 -0700

    x

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,19 +8,17 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t c = 0, size = 0, i = 0;
-	const listint_t *curr;
 
 	if (head == NULL)
 		return (98);
-	size = sizeof(listint_t);
-	curr = head;
-	while (curr != NULL && size > i)
+	size = sizeof(listint_t) * 4;
+	while (head != NULL && size > i)
 	{
-		printf("[%p] %d\n", (void *)curr, curr->n);
-		curr = curr->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 		c++;
 		i++;
 	}
-	printf("-> [%p] %ld\n", (void *)curr, c);
+	printf("-> [%p] %ld\n", (void *)head, c);
 	return (c);
 }
