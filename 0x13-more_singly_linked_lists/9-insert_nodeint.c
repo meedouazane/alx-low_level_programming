9e1d980bfba73b216b08bd237bc74d5217506007
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-01 12:35:38 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,8 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-	if (*head == NULL)
+	if (idx == 0)
 	{
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
