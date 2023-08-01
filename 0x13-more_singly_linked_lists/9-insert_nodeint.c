fa10c9081ebbf8e2d103f566241f5a2a7f797624
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-01 05:42:08 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		curr = curr->next;
 		i++;
+		if (curr == NULL)
+			return (NULL);
 	}
 	new_node->next = curr->next;
 	curr->next = new_node;
