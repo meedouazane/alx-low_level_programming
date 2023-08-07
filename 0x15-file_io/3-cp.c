a42daa56d837c9e3d746eb08461b26906480d8af
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 11:46:03 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,17 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	ff = open(file_from, O_RDONLY);
+	if (ff == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	ft = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (ft == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	while ((rd = read(ff, buffer, sizeof(buffer))) > 0)
 	{
 		wr = write(ft, buffer, rd);
