90eff17abe0bab2039f7608a2b990e904457660e
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 12:24:36 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t ap = 0, length;
 
 	length = _strlen(text_content);
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
