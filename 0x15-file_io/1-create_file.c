25802af3632e43aa29f4be801512f4f51ed9cccb
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 13:42:16 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * _strlen - return length of a string.
- * @s: the string.
- * Return: len length of string.
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != 0)
-	{
-	len++;
-	s++;
-	}
-	return (len);
-}
-/**
  * create_file- creates a files.
  * @filename: the name of the file to create.
  * @text_content: NULL terminated string to write to the file.
@@ -27,10 +11,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		for (length = 0; text_content[length];length)
+			;
 	}
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
