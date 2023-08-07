2d0e28678becefde120cb8c7f52538c90539d2c4
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 16:46:08 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,10 +5,9 @@
  * @av: array of arg being passed.
  * Return: Always 0.
  */
-#define LOCAL_BUFFER 1024
 int main(int ac, char **av)
 {
-	char buffer[LOCAL_BUFFER];
+	char buffer[1024];
 	int ff = 0, ft = 0, wr = 0, rd = 0;
 
 	if (ac != 3)
@@ -17,11 +16,11 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	ff = open(av[1], O_RDONLY);
-	if (ff == -1)
+/*	if (ff == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}
+	}*/
 	ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((rd = read(ff, buffer, 1024)) > 0)
 	{
