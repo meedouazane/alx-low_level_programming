cf70a841b629bc7e853a3e91f4a032651565e8bc
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 12:23:50 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,12 +34,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (length)
-	ap = write(fd, text_content, length);
-	if (ap == -1)
-	{
-		close(fd);
-		return (-1);
-	}
+		ap = write(fd, text_content, length);
 	close(fd);
 	return (ap == length ? 1 : -1);
 }
