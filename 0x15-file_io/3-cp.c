3bd785e322550b6d650cb1aad6287a49ffbfcb25
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 13:49:29 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,32 +7,29 @@
  */
 int main(int ac, char **av)
 {
-	char *file_from = av[1];
-	char *file_to = av[2];
-	int ff, ft;
-	ssize_t wr, rd;
-	char buffer[1024];
+	char *file_from = av[1], *file_to = av[2], buffer[1024];
+	int ff, ft, wr, rd;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to");
-		exit(98);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
 	}
 	if (file_from == NULL)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	ff = open(file_from, O_RDONLY);
 	if (ff == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	ft = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (ft == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	while ((rd = read(ff, buffer, sizeof(buffer))) > 0)
@@ -40,18 +37,13 @@ int main(int ac, char **av)
 		wr = write(ft, buffer, rd);
 		if (wr == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
-	if (close(ff) == -1)
+	if (close(ff) == -1 || close(ft) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", ff);
-		exit(100);
-	}
-	if (close(ft) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", ft);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
 		exit(100);
 	}
 	return (0);
