5cfc8f92f842615b509cd5d0340d50fc9e2d94f8
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 12:50:28 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,11 +30,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
-	if (length)
-		wr = write(f, text_content, length);
+	wr = write(f, text_content, length);
+	if (wr == -1)
+		return (-1);
 	close(f);
-	return (wr == length ? 1 : -1);
+	return (1);
 }
