b3388019cc377d6c962aa6f92f3ad753b1cbb20b
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-08-07 16:20:03 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,34 +5,29 @@
  * @av: array of arg being passed.
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
 	char buffer[1024];
 	int ff = 0, ft = 0, wr = 0, rd = 0;
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	ff = open(argv[1], O_RDONLY);
+	ff = open(av[1], O_RDONLY);
 	if (ff == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	ft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (ft == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((rd = read(ff, buffer, 1024)) > 0)
 	{
 		wr = write(ft, buffer, rd);
-		if (wr == -1)
+		if (ft == -1 || wr == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
