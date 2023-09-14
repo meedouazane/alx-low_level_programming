d816993ae27c8f603b48a1146f77a569c94eb58e
meedouazane <mohamedouazane@yahoo.com>
2023-09-14 15:14:54 -0700

    102

--- 0x17-doubly_linked_lists/10-palindromes.py
+++ 0x17-doubly_linked_lists/10-palindromes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 p = 0
 large = 0
-for i in range(100, 999):
-    for j in range(100, 999):
+for i in range(100, 1000):
+    for j in range(100, 1000):
         p = i * j
         p = str(p)
         if p == p[::-1]:
