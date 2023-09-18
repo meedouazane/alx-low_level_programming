d0cb8980b68e83242ea5ea25788a79e2356edae6
meedouazane <mohamedouazane@yahoo.com>
2023-09-18 06:08:37 -0700

    101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/meedouazane/alx-low_level_programming/blob/master/0x18-dynamic_libraries/injection.so
-LD_PRELOAD=/tmp/injection.so
+wget https://github.com/meedouazane/alx-low_level_programming/blob/master/0x18-dynamic_libraries/injection.so
+LD_PRELOAD=./injection.so
