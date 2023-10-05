1ff2c92fdb7819aed0e7b8c98d9549bca709407c
meedouazane <mohamedouazane@yahoo.com>
2023-10-05 05:20:21 -0700

    01

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array
+ * Return: the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht;
+	unsigned long int i;
+
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
+	new_ht->size = size;
+	new_ht->array = malloc(sizeof(hash_table_t *) * size);
+	if (new_ht->array == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_ht->array[i] = NULL;
+
+	return (new_ht);
+}
+
