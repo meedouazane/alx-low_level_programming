e7ad4c3bdacea24e43971edacc8235b0ccade4cc
meedouazane <mohamedouazane@yahoo.com>
2023-10-05 08:14:42 -0700

    4

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: hash table
+ * Return: Always 0
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+
+	current = ht->
