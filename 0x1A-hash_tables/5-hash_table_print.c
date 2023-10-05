32152275be2a478b9ee95a7e9fdcbffcc51053aa
meedouazane <mohamedouazane@yahoo.com>
2023-10-05 15:07:28 -0700

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,19 +7,22 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	hash_node_t *current;
 	int comma = 0;
 
-	if (ht == NULL || ht->array == NULL)
+	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		current = ht->array[i];
+		while (current)
 		{
 			if (comma)
 				printf(", ");
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			comma = 1;
+			current = current->next;
 		}
 	}
 	printf("}\n");
