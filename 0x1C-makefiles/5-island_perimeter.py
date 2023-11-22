818470b7844a2f09c092aa2b0bff4095b94e4fe4
meedouazane <mohamedouazane@yahoo.com>
2023-11-22 12:46:46 -0800

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' returns the perimeter of the island described in grid '''
+
+
+def island_perimeter(grid):
+    ''' grid is a list of list of integers '''
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i][j + 1] == 0:
+                    p = p + 1
+                if grid[i][j - 1] == 0:
+                    p = p + 1
+                if grid[i + 1][j] == 0:
+                    p = p + 1
+                if grid[i - 1][j] == 0:
+                    p = p + 1
+    return p
